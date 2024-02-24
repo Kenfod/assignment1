a4d6ae3ff7cb7ec451027fdc3f72628d2df0cea7
Kenfod <kelvinnyagudi@yahoo.com>
2024-02-25 00:22:42 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,15 @@
+# Ask user for their name
+print("What is your name? ")
+name=input()
+
+# Ask user for their age
+print("How old are you? ")
+age=input()
+
+# Ask user for their location
+print("Where are you located? ")
+location = input()
+
+# Print personalized message
+print("Hello",name,"! You are", age," years old and located in", location,".")
+
